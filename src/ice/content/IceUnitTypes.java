1ac95e02eaa21e809dcecb43fc04f655f38ce1ef
LostCoedEE <yuliya.palivoda@gmail.com>
2025-04-28 01:29:52 +0300

    test

--- src/ice/content/IceUnitTypes.java
+++ src/ice/content/IceUnitTypes.java
@@ -645,7 +645,7 @@ public class IceUnitTypes {
             rotateSpeed = 1.12f;
             health = 21600;
             armor = 21f;
-            crushDamage = 3.5f;
+            crushDamage = 5f;
             itemCapacity = 0;
             drawCell = false;
             researchCostMultiplier = 0f;
